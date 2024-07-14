f10e29d7faf39d1ac4776f65ee48fa52fca6340d
DHARM <opdsbanasya@gmail.com>
2024-07-14 21:17:42 +0530

    opeartors

---
+++ day2.js
@@ -0,0 +1,76 @@
+// Day 2: 
+// Operators Tasks / Activities: 
+// Activity 1: Arithmetic Operations 
+    console.log("Activity 1:");
+    // • Task 1: Write a program to add two numbers and log the result to the console. 
+        const num1 = 5;
+        const num2 = 13;
+        const sum = num1 + num2;
+        console.log(`sum : ${sum}`);
+
+    // • Task 2: Write a program to subtract two numbers and log the result to the console. 
+        const subtraction = num2 - num1;
+        console.log(`substraction : ${subtraction}`);
+
+    // • Task 3: Write a program to multiply two numbers and log the result to the console. 
+        const multiply = num1 * num2;
+        console.log(`Multiplication : ${multiply}`);
+
+    // • Task 4: Write a program to divide two numbers and log the result to the console. 
+        const divide = num2 / num1;
+        console.log(`Divide : ${divide}`);
+
+    // • Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console.
+        const remainder = num2 % num1;
+        console.log(`Remainder : ${remainder}`);
+
+
+// Activity 2: Assignment Operators
+    console.log("\nActivity 2:");
+    // • Task 6: Use the += operator to add a number to a variable and log the result to the console. 
+        var numVar = 23;
+        numVar += 15;
+        console.log(`Result by += ${numVar}`);
+
+    // • Task 7: Use the -= operator to subtract a number from a variable and log the result to the console.
+        numVar -= 11;
+        console.log(`Result by -= ${numVar}`)
+
+
+// Activity 3: Comparison Operators 
+    console.log("\nActivity 3:");
+    // • Task 8: Write a program to compare two numbers using > and < and log the result to the console. 
+        console.log(` 4 > 2 : ${4>2}`);
+        console.log(` 4 < 2 : ${4<2}`);
+
+    // • Task 9: Write a program to compare two numbers using >= and <= and log the result to the console. 
+        console.log(`12 >= 13 : ${12>=13}`);
+        console.log(`12 <= 12 : ${12<=12}`);
+
+    // • Task 10: Write a program to compare two numbers using == and === and log the result to the console.
+        console.log(`12 == '12 : ${12 == '12'}`)
+        console.log(`12 === '12 : ${12 == '12'}`)
+
+
+// Activity 4: Logical Operators 
+    console.log("\nActivity 4:"); 
+    // • Task 11: Write a program that uses the && operator to combine two conditions and log the result to the console. 
+        console.log(`result by && for 8>2 && 6>2 : ${8>2 && 6>2}`);
+    // • Task 12: Write a program that uses the || operator to combine two conditions and log the result to the console. 
+        console.log(`result by && for 8>2 || 6>2 : ${8>2 || 6>2}`);
+
+    // • Task 13: Write a program that uses the ! operator to negate a condition and log the result to the console.
+        console.log(`result by && for !true : ${!true}`)
+
+
+// Activity 5: Ternary Operator
+    console.log("\nActivity 5:"); 
+    // • Task 14: Write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console. 
+        const checkNum = 56;
+        console.log(`The num ${checkNum} is ${checkNum>=0 ? "Positive": "negative"}`);       
+
+// Feature Request:
+    // 1. Arithmetic Operations Script: Write a script that performs basic arithmetic operations(addition, subtraction, multiplication, division, remainder) on two numbers and logs the results. 
+    // 2. Comparison and Logical Operators Script: Create a script that compares two numbers using different comparison operators and combines conditions using logical operators, logging the results.
+    // 3. Ternary Operator Script: Write a script that uses the ternary operator to determine if a number is positive or negative and logs the result.
+
