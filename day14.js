14dada07c542011775ab04af4418c63373410b61
DHARM <opdsbanasya@gmail.com>
2024-07-26 20:52:30 +0530

    day 14

--- day14.js
+++ day14.js
@@ -2,13 +2,61 @@
 // Tasks/Activities:
 // Activity 1: Class Definition 
     // Task 1: Define a class Person with properties name and age, and a method to return a greeting message. Create an instance of the class and log the greeting message.
+    class Person{
+        constructor(name, age){
+            this.name = name;
+            this.age = age;
+        }
+        greet(){
+            return `Hello ${this.name}, you are ${this.age} year old.`;
+        }
+        updateAge(currAge){
+            this.age = currAge;
+            return this.age;
+        }
+    }
+    const person1 = new Person("Surya", 30);
+    //* console.log(person1.greet());
+
     // Task 2: Add a method to the Person class that updates the age property and logs the updated age.
+    //* console.log(`Your updated age is ${person1.updateAge(35)}`);
+
+
 // Activity 2: Class Inheritance
     // Task 3: Define a class Student that extends the Person class. Add a property studentId and a method to return the student ID. Create an instance of the Student class and log the student ID.
+    class Student extends Person{
+        constructor(name, age, studentId){
+            super(name, age);
+            this.studentId = studentId;
+        }
+        stIdReturn(){
+            return `Your id is ${this.studentId}.`
+        }
+        greet(){
+            return `Hello ${this.name}, you are ${this.age} year old and your Id is ${this.studentId}.`;
+        }
+    }
+    const student1 = new Student("Jai Shah", 15, 235);
+    //* console.log(student1.stIdReturn());
+
     // Task 4: Override the greeting method in the Student class to include the student ID in the message. Log the overridden greeting message.
+    //* console.log(student1.greet());
+
 // Activity 3: Static Methods and Properties
     // Task 5: Add a static method to the Person class that returns a generic greeting message. Call this static method without creating an instance of the class and log the message.
+    class Person1{
+        constructor(name, age){
+            this.name = name;
+            this.age = age;
+        }
+        static greet(){
+            return `Hello, welcome to the class.`;
+        }
+    }
+    console.log(Person1.greet());
+    
     // Task 6: Add a static property to the Student class to keep track of the number of students created. Increment this property in the constructor and log the total number of students.
+    
 // Activity 4: Getters and Setters
     // Task 7: Add a getter method to the Person class to return the full name (assume a firstName and lastName property). Create an instance and log the full name using the getter.
     // Task 8: Add a setter method to the Person class to update the name properties (firstName and lastName). Update the name using the setter and log the updated full name.
